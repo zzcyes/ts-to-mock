765e2f65864623537795517fa1f33a5ae57cc058
oyeitsmeow <zhongzichenn@gmail.com>
2024-09-07 15:34:00 +0800

    chore: add build base dir

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/projects/ts-to-mock/", // 添加这一行
   server: {
     port: 3000, // 或其他可用端口
     host: true,
@@ -13,5 +14,10 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
   },
 });
